f0cb0ecedb2efa099269e5785982ef50040c62e8
yangyi.xie <yangyi.xie@husor.com>
2016-08-01 12:47:15 +0800

    test ImageLoader

--- app/src/main/java/com/example/xieyangyi/framedemo/MainActivity.java
+++ app/src/main/java/com/example/xieyangyi/framedemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.xieyangyi.framedemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.example.xieyangyi.framesdk.imageloader.ImageLoader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        ImageView ivImg = (ImageView) findViewById(R.id.iv_img);
+        String url = "http://b3.hucdn.com/upload/face/1605/11/55695347863284_414x414.jpg";
+        ImageLoader.with(this).load(url).into(ivImg);
     }
 }
