20b3d21d41a5b03e5235f766ea248ebe3410f19c
yangyi.xie <yangyi.xie@husor.com>
2016-08-01 12:46:00 +0800

    反射方式获取glideRequest

--- app/src/main/java/com/example/xieyangyi/framesdk/imageloader/ImageRequestFactory.java
+++ app/src/main/java/com/example/xieyangyi/framesdk/imageloader/ImageRequestFactory.java
@@ -2,23 +2,47 @@ package com.example.xieyangyi.framesdk.imageloader;
 
 import android.content.Context;
 
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * Created by xieyangyi on 16/7/30.
  */
 public class ImageRequestFactory {
 
     private static final String TAG = "image_request_factory";
+    private static final String GLIDE_CLASS_NAME = "com.example.xieyangyi.framesdk.imageloader.ImageRequestGlide";
 
     public static ImageRequestImpl create(Context context) {
 
         switch (ImageCfg.getInstance(context).getSdkType()) {
 
             case ImageCfg.LOADER_TYPE_GLIDE:
-                return ImageRequestGlide.getInstance();
+                return getImpl(context, GLIDE_CLASS_NAME);
+
             case ImageCfg.LOADER_TYPE_FRESCO:
             case ImageCfg.LOADER_TYPE_PICASSO:
             default:
-                return ImageRequestGlide.getInstance();
+                return getImpl(context, GLIDE_CLASS_NAME);
         }
+    }
+
+    private static ImageRequestImpl getImpl(Context context, String className) {
+        ImageRequestImpl impl = null;
+
+        try {
+            ClassLoader loader = context.getClassLoader();
+            Class<? extends ImageRequestImpl> clazz = loader.loadClass(className).asSubclass(ImageRequestImpl.class);
+            impl = (ImageRequestImpl) clazz.getDeclaredMethod("getInstance").invoke(null, (Object[]) null);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return impl;
     }
 }
